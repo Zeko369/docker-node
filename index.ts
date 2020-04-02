f45e714c34bc6059fe4a50f2a55e78725a34ec11
Fran Zekan <zekan.fran369@gmail.com>
2020-04-02 23:41:36 +0200

    Add prisma

--- index.ts
+++ index.ts
@@ -1,17 +1,24 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import morgan from "morgan";
 
 const PORT = 3000;
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.json({ hello: "world" });
+  prisma.user
+    .findMany({ include: { posts: true } })
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.status(500).json({ err }));
 });
 
 app.listen(PORT, () => {
